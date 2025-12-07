242e16bfa446b6c155943c54656d4dcc075b408e
jon-paulo <joaopauloribeiro383@gmail.com>
2025-12-07 17:14:42 -0300

    Create malloc-4.c

---
+++ slide-10-malloc/malloc-4.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+struct Caixa{
+    int valor;
+    struct Caixa* prox;
+};
+
+int contem (struct Caixa* lista, int valor){
+	while(lista != NULL){
+		if (lista->valor == valor){
+			return 1;
+		}
+		lista = lista->prox;
+	}
+	return 0;
+}
+
+void exibe (struct Caixa* caixa){
+	while(caixa != NULL){
+		if (caixa->prox != NULL){
+			printf("%d -> ", caixa->valor);
+		} else {
+			printf("%d \n", caixa->valor);
+		}
+		caixa = caixa->prox;
+	}
+}
+
+int main(){
+
+struct Caixa c5 = {5, NULL};
+struct Caixa c4 = {7, &c5};
+struct Caixa c3 = {9, &c4};
+struct Caixa c2 = {3, &c3};
+struct Caixa c1 = {1, &c2};
+
+struct Caixa* cabeca = &c1;
+c2.prox = &c4;
+if (contem(cabeca, 9)) printf("contem 9: \n");
+exibe(cabeca);
+cabeca = &c3; // botando indice 2 no inicio da lista
+c3.prox = &c1;
+if (contem(cabeca, 9)) printf("contem 9: \n");
+exibe(cabeca);
+
+}
